386021390cf8123a2159a037a09f2c227adaf691
tama-repository <epic6638@gmail.com>
2023-02-06 09:52:09 +0700

    add more userEvent test

--- src/components/counter/Counter.test.tsx
+++ src/components/counter/Counter.test.tsx
@@ -9,8 +9,19 @@ describe("Counter", () => {
     const countElement = screen.getByRole("heading");
     expect(countElement).toBeInTheDocument();
 
-    const buttonElement = screen.getByRole("button", { name: /increment/i });
-    expect(buttonElement).toBeInTheDocument();
+    const incrementElement = screen.getByRole("button", { name: /increment/i });
+    expect(incrementElement).toBeInTheDocument();
+
+    const setNumberElement = screen.getByRole("button", {
+      name: /set number/i,
+    });
+    expect(setNumberElement).toBeInTheDocument();
+
+    const resetElement = screen.getByRole("button", { name: /reset/i });
+    expect(resetElement).toBeInTheDocument();
+
+    const inputElement = screen.getByRole("spinbutton");
+    expect(inputElement).toBeInTheDocument();
   });
 
   function userEventSetup(jsx: React.ReactElement) {
@@ -33,5 +44,51 @@ describe("Counter", () => {
 
     await user.click(buttonElement);
     expect(countElement).toHaveTextContent("1");
+  });
+
+  test("add 10 to count if set number 10", async () => {
+    const { user } = userEventSetup(<Counter />);
+
+    const setNumberElement = screen.getByRole("button", {
+      name: /set number/i,
+    });
+    const countElement = screen.getByRole("heading");
+    const amountInputElement = screen.getByRole("spinbutton");
+
+    await user.type(amountInputElement, "10");
+    expect(amountInputElement).toHaveValue(10);
+
+    await user.click(setNumberElement);
+    expect(countElement).toHaveTextContent("10");
+  });
+
+  test("element focused in the right order", async () => {
+    const { user } = userEventSetup(<Counter />);
+
+    const incrementElement = screen.getByRole("button", {
+      name: /increment/i,
+    });
+    const inputElement = screen.getByRole("spinbutton");
+    const setNumberElement = screen.getByRole("button", {
+      name: /set number/i,
+    });
+    const resetElement = screen.getByRole("button", { name: /reset/i });
+
+    const userTab = async (element: HTMLElement) => {
+      await user.tab();
+      expect(element).toHaveFocus();
+    };
+
+    // await user.tab();
+    // expect(incrementElement).toHaveFocus();
+    // await user.tab();
+    // expect(inputElement).toHaveFocus();
+    // await user.tab();
+    // expect(setNumberElement).toHaveFocus();
+
+    await userTab(incrementElement);
+    await userTab(inputElement);
+    await userTab(setNumberElement);
+    await userTab(resetElement);
   });
 });
--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -2,13 +2,37 @@ import { useState } from "react";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
+  const [numberValue, setNumberValue] = useState<number>(0);
 
   const incrementHandler = () => {
     setCount((prev) => prev + 1);
   };
 
+  const numberChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNumberValue(+event.target.value);
+  };
+
+  const numberSetHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!numberValue || numberValue === 0) {
+      return;
+    }
+
+    if (numberValue) {
+      setCount((prev) => prev + +numberValue);
+    }
+
+    setNumberValue(0);
+  };
+
+  const resetHandler = () => {
+    setCount(0);
+    setNumberValue(0);
+  };
+
   return (
-    <div
+    <section
       style={{
         display: "flex",
         flexDirection: "column",
@@ -18,7 +42,18 @@ export default function Counter() {
       }}
     >
       <h1>{count}</h1>
-      <button onClick={incrementHandler}>Increment</button>
-    </div>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <button onClick={incrementHandler}>Increment</button>
+        <form onSubmit={numberSetHandler}>
+          <input
+            type="number"
+            onChange={numberChangeHandler}
+            value={numberValue}
+          />
+          <button>Set number</button>
+        </form>
+        <button onClick={resetHandler}>Reset</button>
+      </div>
+    </section>
   );
 }
